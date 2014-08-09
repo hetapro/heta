40bb5e6879aec19bb4ae50ac0107ef768cd8ca02
Heta Hathi <Heta@Heta.promact.com>
2014-08-09 15:57:05 +0530

    poll application

---
+++ polls/tests.py
@@ -0,0 +1,62 @@
+simport datetime
+from django.utils import timezone
+from django.test import TestCase
+
+ # Create your tests here.
+
+from polls.models import Poll
+from django.core.urlresolvers import reverse
+
+def create_poll(question, days):
+   return Poll.objects.create(questions=questions, pub_date=timezone.now()+datetime.timedelta(days=days))
+
+
+class PollViewTests(TestCase):
+   def test_index_view_with_no_poll(self):
+      """If no polls exist, an appropriate message should be displayed."""
+      response=self.client.get(reverse('polls:index'))
+      self.assertEqual(response.status_code, 200)
+      self.assertContains(response, "No polls are available.")
+      self.assertQuerysetEqual(response.context['latest_poll_list'],[])
+
+   def test_index_view_with_a_past_poll(self):
+      create_poll(questions="Past poll.", days=-30)
+      response=self.client.get(reverse('polls:index'))
+      self.assertQuerysetEqual(response.context['latest_poll_list'], ['<Poll: Past poll.>'])
+   
+   def test_index_view_with_a_future_poll(self):
+      create_poll(questions="Future poll.", days=30)
+      response=self.client.get(reverse('polls:index'))
+      self.assertContains(response, "No polls are available.", status_code=200)
+      self.assertQuerysetEqual(response.context['latest_poll_list'], ['<Poll: Past poll.>'])
+
+   def test_index_view_with_future_poll_and_past_poll(self):
+      create_poll(questions="Past poll.", days=-30)
+      create_poll(questions="Future poll.", days=30)
+      response=self.client.get(reverse('polls:index'))
+      self.assertQuerysetEqual(response.context['latest_poll_list'], ['<Poll:Past poll.>'])
+
+   def test_index_view_with_two_past_polls(self):
+      create_poll(questions="Past poll 1.", days=-30)
+      create_poll(questions="Past poll 2.", days=-5)
+      response=self.client.get(reverse('polls:index'))
+      self.assertQuerysetEqual(response.context['latest_poll_list'], ['<Poll:Past poll2.>'] ,['<Poll:Past poll1.>'])
+
+class PollIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_poll(self):
+        """
+        The detail view of a poll with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_poll = create_poll(question='Future poll.', days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_poll.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_poll(self):
+        """
+        The detail view of a poll with a pub_date in the past should display
+        the poll's question.
+        """
+        past_poll = create_poll(question='Past Poll.', days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_poll.id,)))
+        self.assertContains(response, past_poll.question, status_code=200)
---
+++ polls/views.py
@@ -0,0 +1,55 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+#from django.template import RequestContext, loader
+#from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+
+from polls.models import Poll, Choice
+class IndexView(generic.ListView):
+   template_name= 'polls/index.html'
+   context_object_name= 'latest_poll_list'
+
+   def get_queryset(self):
+      return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+   model=Poll
+   template_name='polls/details.html'
+   def get_queryset(self):
+      return Poll.objects.filter(pub_date__lte=timezone.now())
+
+class ResultView(generic.DetailView):
+   model=Poll
+   template_name='polls/results.html'
+
+
+"""def index(request):
+   #return HttpResponse("Hello, world. You are at the poll index.")
+   latest_poll_list=Poll.objects.order_by('-pub_date')[:5]
+   #template=loader.get_template('polls/index.html')
+   #context=RequestContext(request, {'latest_poll_list': latest_poll_list})
+   context={'latest_poll_list': latest_poll_list}
+   #return HttpResponse(template.render(context))
+   return render(request, 'polls/index.html', context)
+
+def detail(request,poll_id):
+   poll=get_object_or_404(Poll,pk=poll_id)
+   return render(request,'polls/details.html', {'poll': poll}) """
+
+def vote(request, poll_id):
+   p=get_object_or_404(Poll, pk=poll_id)
+   try:
+      selected_choice=p.choice_set.get(pk=request.POST['choice'])
+   except(KeyError, Choice.DoesNotExist):
+      return render(request, 'polls/details.html', {'poll':p, 'error_message': "You didn't select a choice.",})
+   else:
+      selected_choice.votes+=1
+      selected_choice.save()
+      return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))   
+
+
+"""def results(request, poll_id):
+   poll=get_object_or_404(Poll, pk=poll_id)
+   return render(request,'polls/results.html', {'poll': poll}) """
